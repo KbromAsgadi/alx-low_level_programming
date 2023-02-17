f5f2c689cdb46ea603296c96fabb586c9799a28f
KbromAsgadi <kbromasgadi92@gmail.com>
2023-02-16 19:48:18 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0(Success)
+*/
+#include <string.h>
+int main(void)
+{
+
+	/*intailizing string*/
+	char str1[] = ("\"Programming is like building a multilingual puzzle");
+
+	puts(str1);
+	return (0);
+}
