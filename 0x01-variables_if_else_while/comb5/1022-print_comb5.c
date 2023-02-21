8b02a774b2078fed65320675c3f1bd8281dffab6
KbromAsgadi <kbromasgadi92@gmail.com>
2023-02-20 16:04:11 -0800

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/comb5/1022-print_comb5.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: 0 (Success)
+*/
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+	int l;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 9; j++)
+		{
+			for (k = 0; k < 10; k++)
+			{
+				for (l = 1; l < 10; l++)
+				{
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(' ');
+					putchar(k + '0');
+					putchar(l + '0');
+					if (i == 9 && j == 8 && k == 9 && l == 9)
+					{
+						continue;
+					}
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
