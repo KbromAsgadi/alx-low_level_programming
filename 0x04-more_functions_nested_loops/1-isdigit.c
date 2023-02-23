c0c4cfcb59f223d8fc72be647304fe36a6462a80
KbromAsgadi <kbromasgadi92@gmail.com>
2023-02-23 12:25:48 -0800

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* _isupper - upper letter
+* @c: chsr to check
+,*
+*Return: 0 or 2
+*/
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
+}
