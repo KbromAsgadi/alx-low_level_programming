9cd91dd612de418118e4fa551f906bb8e855910f
KbromAsgadi <kbromasgadi92@gmail.com>
2023-02-24 09:35:45 -0800

    updated

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,19 +14,26 @@ int main(void)
 		{
 			printf("FizzBuzz ");
 		}
+		else if (i % 5 == 0)
+		{
+			if (i == 100)
+			{
+				printf("buzz");
+				printf("\n");
+			}
+			else
+			{
+				printf("buzz ");
+			}
+		}
 		else if (i % 3 == 0)
 		{
 			printf("Fizz ");
-		}
-		else if (i % 5 == 0)
-		{
-			printf("Buzz ");
 		}
 		else
 		{
 			printf("%d ", i);
 		}
 	}
-	printf("\n");
 	return (0);
 }
