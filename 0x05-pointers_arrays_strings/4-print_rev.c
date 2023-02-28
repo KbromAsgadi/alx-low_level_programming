1666b725ad303c32e1c9b0d391f0b37d527a28ac
KbromAsgadi <kbromasgadi92@gmail.com>
2023-02-27 16:57:40 -0800

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+/**
+*print_rev - function prints reverse
+* @s: string
+*Return: 0
+*/
+void print_rev(char *s)
+{
+	int length = strlen(s);
+	int i;
+
+	i = length - 1;
+	while (i >= 0)
+	{
+		_putchar(i);
+		i--;
+	}
+	_putchar('\n');
+}
+
