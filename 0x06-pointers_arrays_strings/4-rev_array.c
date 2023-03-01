bafc877386d15dc3bf4b8f9873253f88b5bad635
KbromAsgadi <kbromasgadi92@gmail.com>
2023-03-01 13:15:42 -0800

    updated

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -4,22 +4,21 @@
 /**
  * print_array - function that reverses the content of an array of integers
  * @a: an array of integers
- * @n: the number of elements to swap
- *
+ * @n: the number of elements to revers
  * Return: nothing.
  */
 void reverse_array(int *a, int n)
 {
 	int i;
 
-	i = 0;
-	while (i < n)
+	i = 1;
+	while (i <= n)
 	{
-		if (i != 0)
+		if (i != 1)
 		{
 			printf(", ");
 		}
-		printf("%d", a[n - i]);
+		printf("%d", a[n-i]);
 		i++;
 	}
 	printf("\n");
