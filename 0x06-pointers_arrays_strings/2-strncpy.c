d162ececa879a7e8b4943bdfa77f59a0b6943820
KbromAsgadi <kbromasgadi92@gmail.com>
2023-03-01 11:04:08 -0800

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+*_strncpy - a function that copies a string up to n bytes
+* @dest: destination string
+* @src: string to be copied
+* @n: amount of byet to be copied
+* Return: dest
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
