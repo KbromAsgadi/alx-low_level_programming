3deabbcb44b3305500ad4c07e3a575211979447c
KbromAsgadi <kbromasgadi92@gmail.com>
2023-03-08 09:04:25 -0800

    1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*_print_rev_recursion - function that prints a string, followed by a new line
+*@s: string
+*Return: s
+*/
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+	else
+		_putchar('\n');
+}
