a1ba03124ebd23fa57da36b5f8fa044b3cbf622e
KbromAsgadi <kbromasgadi92@gmail.com>
2023-03-16 10:27:26 -0700

    new

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -19,10 +19,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	if (n < len2)
-		s = malloc(sizeof(char) * (len1 + n + 1));
-	else
+	if (n >= len2)
 		s = malloc(sizeof(char) * (len1 + len2 + 1));
+	else
+		s = malloc(sizeof(char) * (len1 + n + 1));
 
 	if (!s)
 		return (NULL);
