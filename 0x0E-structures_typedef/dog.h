1d83c079fe53fe8bff28af42f89ac0bb9a13543d
KbromAsgadi <kbromasgadi92@gmail.com>
2023-03-20 13:35:41 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+*struct dog - user defined data type
+*@name: name of person
+*@age: integer type
+*@owner: string
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* _DOG_H_*/
