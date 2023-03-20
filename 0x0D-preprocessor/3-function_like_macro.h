baa85033704f61f598380ea1ab9609717c9c5daf
KbromAsgadi <kbromasgadi92@gmail.com>
2023-03-20 13:32:10 -0700

    updated

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,14 +1,6 @@
-#ifndef _3_FUNCTION_LIKE_MACROL_H_
-#define _3_FUNCTION_LIKE_MACROL_H_
-
-/*
- *absolut value definitions
- *
- * Function prototypes
- */
 
 #ifndef ABS
 
 #define ABS(x) (((x) < 0) ? -(x) : (x))
 
-#endif /* _3_FUNCTION_LIKE_MACROL_H_ */
+#endif
--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,13 +1,6 @@
-#ifndef _4_SUM_H_
-#define _4_SUM_H_
-/**
-*sum - the sum of two variables
-*@x: frist variable
-*@y: second varaible
-*/
 
 #ifndef SUM
 
 #define SUM(x, y) ((x) + (y))
 
-#endif /* _4_SUM_H_ */
+#endif
