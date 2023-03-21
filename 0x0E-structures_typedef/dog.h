68657415a9f658cb655d48f9312eb3012516eaf7
KbromAsgadi <kbromasgadi92@gmail.com>
2023-03-21 07:33:56 -0700

    1-init_dog.c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,6 @@
-#ifndef _DOG_H_
-#define _DOG_H_
+#ifndef DOG_H
+#define DOG_H
+
 /**
 *struct dog - user defined data type
 *@name: name of person
@@ -13,4 +14,16 @@ struct dog
 	char *owner;
 };
 
-#endif /* _DOG_H_*/
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+
+#endif
